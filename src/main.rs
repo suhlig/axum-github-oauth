e4e75d2792f2a3911af34bc5490d2ca40f5d2631
Steffen Uhlig <steffen@familie-uhlig.net>
2025-05-06 20:50:01 +0200

    Show GH login of authenticated user

--- src/main.rs
+++ src/main.rs
@@ -48,7 +48,7 @@ pub async fn home_handler() -> Html<String> {
 }
 
 pub async fn protected_handler(user: User) -> Html<String> {
-    Html(String::from("s3cret"))
+    Html(String::from(format!("Hello {}, here is the s3cret", user.login)))
 }
 
 pub async fn not_found_handler() -> impl IntoResponse {
